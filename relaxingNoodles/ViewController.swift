6df0034bbab4fce647f70f69c2ea0063389d6ca7
Sebastian Tyson <styson@lcs.on.ca>
2020-05-25 10:32:24 -0400

    Initial Commit

---
+++ relaxingNoodles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  relaxingNoodles
+//
+//  Created by Tyson, Sebastian on 2020-05-25.
+//  Copyright © 2020 Tyson, Sebastian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
